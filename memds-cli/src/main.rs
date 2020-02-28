a745f469f844fd88f36cc980f6c37cc453265630
Jeff Garzik <jeff@bloq.com>
2020-02-28 08:01:34 -0500

    Add skeleton CLI package

---
+++ memds-cli/src/main.rs
@@ -0,0 +1,42 @@
+
+extern crate clap;
+
+use clap::{Arg, SubCommand};
+
+const APPNAME: &'static str = "memds-cli";
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn main() {
+    // parse command line
+    let cli_matches = clap::App::new(APPNAME)
+        .version(VERSION)
+	.about("Memds CLI")
+	.subcommand(SubCommand::with_name("str.get")
+	    .about("String.Get")
+	    .arg(Arg::with_name("key")
+	        .help("Key of item to retrieve")
+		.required(true)))
+	.subcommand(SubCommand::with_name("str.set")
+	    .about("String.Set")
+	    .arg(Arg::with_name("key")
+	        .help("Key of item to store")
+		.required(true))
+	    .arg(Arg::with_name("value")
+	        .help("Value of item to store")
+		.required(true)))
+	.get_matches();
+
+    match cli_matches.subcommand() {
+	("str.get", Some(matches)) => {
+	    let key = matches.value_of("key").unwrap();
+	    println!("ACTION: str.get {}", key);
+	},
+	("str.set", Some(matches)) => {
+	    let key = matches.value_of("key").unwrap();
+	    let value = matches.value_of("value").unwrap();
+	    println!("ACTION: str.set {}={}", key, value);
+	},
+        ("", None) => println!("No subcommand specified.  Run with --help for help."),
+	_ => unreachable!(),
+    }
+}
