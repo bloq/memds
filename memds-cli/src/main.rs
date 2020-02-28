4407186637642186d65fc3460e0b339460ff12cd
Jeff Garzik <jeff@bloq.com>
2020-02-28 08:02:32 -0500

    cargo fmt

--- memds-cli/src/main.rs
+++ memds-cli/src/main.rs
@@ -1,4 +1,3 @@
-
 extern crate clap;
 
 use clap::{Arg, SubCommand};
@@ -10,33 +9,41 @@ fn main() {
     // parse command line
     let cli_matches = clap::App::new(APPNAME)
         .version(VERSION)
-	.about("Memds CLI")
-	.subcommand(SubCommand::with_name("str.get")
-	    .about("String.Get")
-	    .arg(Arg::with_name("key")
-	        .help("Key of item to retrieve")
-		.required(true)))
-	.subcommand(SubCommand::with_name("str.set")
-	    .about("String.Set")
-	    .arg(Arg::with_name("key")
-	        .help("Key of item to store")
-		.required(true))
-	    .arg(Arg::with_name("value")
-	        .help("Value of item to store")
-		.required(true)))
-	.get_matches();
+        .about("Memds CLI")
+        .subcommand(
+            SubCommand::with_name("str.get").about("String.Get").arg(
+                Arg::with_name("key")
+                    .help("Key of item to retrieve")
+                    .required(true),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("str.set")
+                .about("String.Set")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to store")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .help("Value of item to store")
+                        .required(true),
+                ),
+        )
+        .get_matches();
 
     match cli_matches.subcommand() {
-	("str.get", Some(matches)) => {
-	    let key = matches.value_of("key").unwrap();
-	    println!("ACTION: str.get {}", key);
-	},
-	("str.set", Some(matches)) => {
-	    let key = matches.value_of("key").unwrap();
-	    let value = matches.value_of("value").unwrap();
-	    println!("ACTION: str.set {}={}", key, value);
-	},
+        ("str.get", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            println!("ACTION: str.get {}", key);
+        }
+        ("str.set", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            let value = matches.value_of("value").unwrap();
+            println!("ACTION: str.set {}={}", key, value);
+        }
         ("", None) => println!("No subcommand specified.  Run with --help for help."),
-	_ => unreachable!(),
+        _ => unreachable!(),
     }
 }
