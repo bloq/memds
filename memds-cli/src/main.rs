92f11e91d8d0bbe6a862a0c4aab2d2f0d7ad22d6
Jeff Garzik <jeff@bloq.com>
2020-03-01 11:42:48 -0500

    CLI: add new string ops

--- memds-cli/src/main.rs
+++ memds-cli/src/main.rs
@@ -11,11 +11,71 @@ fn main() {
         .version(VERSION)
         .about("Memds CLI")
         .subcommand(
+            SubCommand::with_name("decr")
+                .about("String.Decr: Decrement numeric item by 1")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to update")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("decrby")
+                .about("String.DecrBy: Decrement numeric item")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to update")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("n")
+                        .help("Numeric delta for operation")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("incr")
+                .about("String.IncrBy: Increment numeric item by 1")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to update")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("incrby")
+                .about("String.Incr: Increment numeric item")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to update")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("n")
+                        .help("Numeric delta for operation")
+                        .required(true),
+                ),
+        )
+        .subcommand(
             SubCommand::with_name("get")
                 .about("String.Get: Retrieve item")
                 .arg(
                     Arg::with_name("key")
                         .help("Key of item to retrieve")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("getset")
+                .about("String.GetSet: Store item, return old value")
+                .arg(
+                    Arg::with_name("key")
+                        .help("Key of item to retrieve+store")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .help("Value of item to store")
                         .required(true),
                 ),
         )
@@ -36,9 +96,32 @@ fn main() {
         .get_matches();
 
     match cli_matches.subcommand() {
+        ("decr", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            println!("ACTION: str.decr {}", key);
+        }
+        ("decrby", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            let n = matches.value_of("n").unwrap();
+            println!("ACTION: str.decrby {} {}", key, n);
+        }
+        ("incr", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            println!("ACTION: str.incr {}", key);
+        }
+        ("incrby", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            let n = matches.value_of("n").unwrap();
+            println!("ACTION: str.incrby {} {}", key, n);
+        }
         ("get", Some(matches)) => {
             let key = matches.value_of("key").unwrap();
             println!("ACTION: str.get {}", key);
+        }
+        ("getset", Some(matches)) => {
+            let key = matches.value_of("key").unwrap();
+            let value = matches.value_of("value").unwrap();
+            println!("ACTION: str.getset {}={}", key, value);
         }
         ("set", Some(matches)) => {
             let key = matches.value_of("key").unwrap();
